0d3c0e542da39759d614b2724b26fe0b257c12ac
GiroSan <girosan86@gmail.com>
2023-10-03 17:03:12 -0300

    Adicionado Index e contato

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return 
+    nome = "Deus Super Sayajin"
+    dados = {"profissao": "Guerreiro Sayajin", "Raça": "kakaroto"}
+    return render_template('index.html' ,  nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
